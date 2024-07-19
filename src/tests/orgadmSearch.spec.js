dd03faa1a4dd2198ca423a2a23a7029a2ed8bee2
“Murali” <muraligolla502@gmail.com>
2024-07-19 11:37:42 +0530

    added settimeout

--- src/tests/orgadmSearch.spec.js
+++ src/tests/orgadmSearch.spec.js
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 import { OrgLoginPage } from '../pageobjects/OrgLoginPage'
 
 test("My first orange login", async({page}) => {
-
+    test.setTimeout(120000);
     //await page.goto('/')
     const OrgLoginPageobj = new OrgLoginPage(page);
     await OrgLoginPageobj.login('Admin','admin123');
