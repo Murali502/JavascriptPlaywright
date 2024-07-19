4d3432c946b3aef0e3a3485658d7e03c99a6c314
“Murali” <muraligolla502@gmail.com>
2024-07-19 11:58:07 +0530

    updated code

--- src/pageobjects/OrgLoginPage.js
+++ src/pageobjects/OrgLoginPage.js
@@ -16,7 +16,7 @@ exports.OrgLoginPage = class OrgLoginPage{
         await this.page.getByPlaceholder('Password').fill(Password);
         await this.page.getByRole('button', { name: 'Login' }).click();
         await this.page.getByRole('link', { name: 'Admin' }).click();
-        //await this.page.getByRole('textbox').nth(1).click();
+        await this.page.getByRole('textbox').nth(1).click();
         await this.page.getByRole('textbox').nth(1).fill('admin');
         await this.page.locator('form i').first().click();
         await this.page.getByRole('option', { name: 'Admin' }).click();
